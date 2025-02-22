bd68c979e497951dde244ed272fe7c37a0cb0323
Priyansh Gupta <priyansh.dev02@gmail.com>
2025-02-22 16:53:31 +0530

    showing selected language

--- lib/Homescreen/selectlanguage.dart
+++ lib/Homescreen/selectlanguage.dart
@@ -2,7 +2,12 @@ import 'package:aarogya_vishwas/main.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LanguageSelectionScreen extends StatelessWidget {
+class LanguageSelectionScreen extends StatefulWidget {
+  @override
+  _LanguageSelectionScreenState createState() => _LanguageSelectionScreenState();
+}
+
+class _LanguageSelectionScreenState extends State<LanguageSelectionScreen> {
   final Map<String, String> languages = {
     'English': 'en',
     'Hindi': 'hi',
@@ -18,6 +23,21 @@ class LanguageSelectionScreen extends StatelessWidget {
     'Urdu': 'ur',
   };
 
+  String? _selectedLanguageCode;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSelectedLanguage();
+  }
+
+  Future<void> _loadSelectedLanguage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _selectedLanguageCode = prefs.getString('languageCode');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +51,18 @@ class LanguageSelectionScreen extends StatelessWidget {
         children: languages.entries.map((entry) {
           return ListTile(
             title: Text(entry.key),
+            trailing: _selectedLanguageCode == entry.value
+                ? Icon(Icons.check, color: Colors.teal) // Show tick if selected
+                : null, // No tick if not selected
             onTap: () async {
               // Save the selected language code to shared preferences
               SharedPreferences prefs = await SharedPreferences.getInstance();
               await prefs.setString('languageCode', entry.value);
+
+              // Update the UI to show the tick
+              setState(() {
+                _selectedLanguageCode = entry.value;
+              });
 
               // Navigate to the home screen with the selected language
               Navigator.pushReplacement(
